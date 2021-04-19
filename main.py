54d31477a8774be5f970b9b9918ef98a846e89f4
Venkatesh0625 <venkatesan.0625@gmail.com>
2021-04-19 14:39:05 +0530

    add interative output

--- main.py
+++ main.py
@@ -23,3 +23,24 @@ constraints = {
 problem = CSProblem(arcs, domains, constraints)
 r = problem.arc_consistency()  
 
+print("Edge    | New Domain     | Edges to Reconsider")
+print("--------|----------------|--------------------")
+
+for step in r:
+
+    if step == None:
+        print('Inconsistent !')
+
+    else:
+        edge = step[0]
+
+        if edge == None:
+            print('Result : ', step[1])
+    
+        else:
+            x =  edge[0]
+            new_domain = step[1][x]
+            edges_to_reconsider = step[2]
+            print(str(edge) + ' | ' + str(x) + ' = ' + str(new_domain) + ' | ' + str(edges_to_reconsider) + '\n\n')
+
+
